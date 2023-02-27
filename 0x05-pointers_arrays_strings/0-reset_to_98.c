afb22b8f8868c3daa4bc539256022cf2180cf330
softtechlink <oludonaldfm@gmail.com>
2023-02-27 10:22:35 -0800

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,16 +2,13 @@
 
 
 /**
- * reset_to_98 - take a pointer to an int as parameter and updates
- * the value it point to to 98
- * @n: point to an int
- * Return: Nothing
+ * reset_to_98 - update the value of n to 98
+ * @n: input
+ * Return: n
  */
-
 void reset_to_98(int *n);
 
 {
-
 	*n = 98;
 
 }

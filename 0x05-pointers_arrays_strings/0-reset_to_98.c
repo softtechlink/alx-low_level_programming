f4d604bcbdd5db9acfed1bbec093553b916ce9fd
softtechlink <oludonaldfm@gmail.com>
2023-02-27 10:07:50 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - take a pointer to an int as parameter and updates
+ * the value it point to to 98
+ * @n: point to an int
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n);
+
+{
+
+	*n = 98;
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -10,3 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
+
+#endif
